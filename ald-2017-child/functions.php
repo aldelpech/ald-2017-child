27ca7244166a4bbbe57e71a83bc55c05b29e91ed
Anne-Laure Delpech <contact@parcours-performance.com>
2017-07-03 09:40:42 +0200

    header image resized, homepage sections added

--- ald-2017-child/functions.php
+++ ald-2017-child/functions.php
@@ -1,7 +1,13 @@
 <?php 
-add_action( 'wp_enqueue_scripts', 'tw17_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'al17_child_enqueue_styles' );
 
-function tw17_child_enqueue_styles() { 
+add_filter( 'twentyseventeen_custom_header_args', 'al17_custom_header_args' );
+
+// 6 sections dans la page d'accueil (au lieu de 4)
+add_filter( 'twentyseventeen_front_page_sections', function(){ return 6; } );
+
+
+function al17_child_enqueue_styles() { 
 
     $parent_style = 'twentyseventeen-style-css'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
@@ -13,5 +19,15 @@ function tw17_child_enqueue_styles() {
     );
 
 } 
+
+function al17_custom_header_args( $args ) {
+	
+	// source https://kinsta.com/blog/twenty-seventeen-theme/
+	$args['default-image'] = get_theme_file_uri( '/images/al_domotic-2000x350.png' );
+	$args['height'] = 350 ;
+	return $args;
+}
+
+
 
 ?>
